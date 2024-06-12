7d3195d06854060ab1a35e3ace89672c0edd1447
Daniel Dybing <daniel.dybing@gmail.com>
2024-06-12 13:18:49 +0200

    Removed old code

--- download_forks.py
+++ download_forks.py
@@ -1,39 +1,14 @@
 import json
 import requests
 import time
-from argparse import ArgumentParser
 import os
 import subprocess
 
-# Parse arguments
-parser = ArgumentParser(description="Send webhooks to Jenkins")
-parser.add_argument('--max', type=int, default=-1, help="Maximum number of webhooks to send")
-parser.add_argument('--pause', type=float, default=10, help="Number of seconds to wait between each webhook")
-parser.add_argument('--message', type=str, default="[Test] Webhook", help="Commit message")
-parser.add_argument('--repository_url', type=str, default="https://github.com/uiaict/2024-ikt218-osdev")
-
-args = parser.parse_args()
-
-# Maximum amount of webhooks to send
-max_to_send = args.max
-
-# Pause between each webhook
-pause = args.pause
-
-# Commit message
-message = args.message
-
-# Number of webhooks sent
-sent = 0
-
 # Repository URL
-repository_url = args.repository_url
+repository_url = "https://github.com/uiaict/2024-ikt218-osdev"
 
 owner = repository_url.split('/')[3]
 repo = repository_url.split('/')[4]
-
-# Webhook endpoint URL
-webhook_url = 'http://atos.ddns.net:30080/jenkins/generic-webhook-trigger/invoke?token=ikt218'
 
 
 print("Fetching forks list for repo: ", repository_url )
